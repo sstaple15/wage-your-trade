b9e9cfb7d144eb67553b56b6b9801ae8e412cce2
sstaple15 <43829535+sstaple15@users.noreply.github.com>
2019-03-13 09:30:17 -0500

    Add in more dats for use

--- choropleth.js
+++ choropleth.js
@@ -4,10 +4,17 @@ async function choropleth(data) {
 
   // create linear and color scales for choropleth
   const jobDomain = computeDomain(cty_stuff, "ag_jobs");
-  var cty_jobs = cty_stuff.reduce((acc, row) => {
-    acc[row.id] = row.ag_jobs;
+  var cty_jobs = cty_stuff.reduce((acc, d) => {
+    acc[d.id] = { 'agr' : d.ag_jobs,
+                  'man' : d.man_jobs,
+                  'min' : d.min_jobs,
+                  'edu' : d.ed_jobs,
+                  'eng' : d.eng_jobs,
+                  'bus' : d.bus_jobs,
+                  'its' : d.it_jobs,
+                  'fin' : d.fin_jobs };
     return acc;
-  }, {});
+  });
   const jobScale = d3.scaleLinear().domain([0, jobDomain.max]).range([0,1])
   const colorScale = d => d3.interpolateYlGnBu(jobScale(d));
 
@@ -17,7 +24,7 @@ async function choropleth(data) {
       .offset([-10, 0])
       .direction('n')
       .html(function (d) {
-        text = 'Supported Jobs Rate: ' + Math.round(cty_jobs[d.id]);
+        text = 'Supported Jobs Rate: ' + Math.round(cty_jobs[d.id].agr);
         return text;
   });
   svg.call(tip);
@@ -37,10 +44,13 @@ async function choropleth(data) {
       d3.select(this).classed('active', false)
     })
     .attr("fill", (d) => {
-      return colorScale(cty_jobs[d.id]);
+      try {
+        colorValue = colorScale(cty_jobs[d.id].agr);
+      } catch {}
+      return colorValue;
     })
-    .attr("d", path)
-    .append("title");
+    .attr("d", path);
+    // .append("title");
 
   svg.append("path")
       .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
